3c4481405673521e6a15061f46a37c880631922c
MakotoMiyamoto <makotonomeru@gmail.com>
2021-04-05 19:26:32 -0500

    encapsulated global vars in class

--- src/app.js
+++ src/app.js
@@ -11,28 +11,37 @@ const env_config = dotenv.config({
     // set env filename to your env (probably .env)
     path: path.join(__dirname, 'config', 'dev.env'),
 });
-const config = EnvironmentConfiguration.loadFromENV(env_config);
 
-// initialize logger and set log folder location
-const logger = new Logger(path.join(__dirname, '..', 'logs'));
+// this is done so data from the main script can be
+// passed by reference to event handlers and such.
+class Bot extends Discord.Client {
+    /**
+     * @param {dotenv.DotenvConfigOutput} env_config
+     * @param {Discord.ClientOptions} options
+     */
+    constructor(env_config = dotenv.config(), options) {
+        super(options);
+        this.config = EnvironmentConfiguration.loadFromENV(env_config);
+        // initialize logger and set log folder location
+        this.logger = new Logger(path.join(__dirname, '..', 'logs'));
+        // setup exit handler. callback will execute upon program death.
+        this.exitHandler = new ExitHandler(() => {
+            this.logger.dumpLogs();
+        });
+        this.exitHandler.initialize();
+    }
+    async login() {
+        return super.login(this.config.token).catch((reason) => {
+            this.logger.error('Your bot could not be logged in.');
+            this.logger.error(reason);
+            // use this to avoid having to set process.exitCode manually
+            this.exitHandler.exit(1);
+        });
+    }
+}
 
-// setup exit handler. callback will execute upon program death.
-const exitHandler = new ExitHandler(() => {
-    logger.dumpLogs(); // write log file for current process
-});
-exitHandler.initialize();
-
-// initialize discord client
-const client = new Discord.Client();
-
-client.login(config.token).catch((reason) => {
-    logger.error('Your bot could not be logged in.');
-    logger.error(reason);
-    // use this to avoid having to set process.exitCode manually
-    exitHandler.exit(1);
-});
-
-client.once('ready', () => {
-    let s = `${client.user.username}#${client.user.discriminator} is online!`;
-    logger.info(s);
+const bot = new Bot(env_config);
+bot.login().then(() => {
+    const s = `${bot.user.username}#${bot.user.discriminator} is online!`;
+    bot.logger.info(s);
 });
